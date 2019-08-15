d6e7cc5461e8c620eb103d91baef2f6a3672e355
glofishh <gloriaulee@gmail.com>
2019-08-15 11:32:52 -0400

    whoops ignored build

---
+++ build/precache-manifest.a1fb90c4bea32ca7010e25fbe65af4d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863fab0cfea4b032ed6a1b9e00582844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3f31388912cf9349ec",
+    "url": "/static/css/main.de5ae4fa.chunk.css"
+  },
+  {
+    "revision": "43802a088b510919226c",
+    "url": "/static/js/2.0c3d7aa3.chunk.js"
+  },
+  {
+    "revision": "ee3f31388912cf9349ec",
+    "url": "/static/js/main.ed3d0c68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bf9f5d50c1b928ff21436517a1a95ad9",
+    "url": "/static/media/ProximaNova-Regular.bf9f5d50.otf"
+  },
+  {
+    "revision": "f14eee643541cf03a10f26c944cc29f5",
+    "url": "/static/media/ProximaNova-Semibold.f14eee64.otf"
+  }
+]);
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-//production mode
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname, 'build', 'index.html'));
-  });
-};
+// //production mode
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'build')));
+//   app.get('*', (req, res) => {
+//     res.sendfile(path.join(__dirname, 'build', 'index.html'));
+//   });
+// };
 
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));})
+// //build mode
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/public/index.html'));})
 
-//start server
-app.listen(PORT, () => {
-  console.log( `server listening on port: ${PORT}`);
-})
-
-
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// });
-
+// //start server
 // app.listen(PORT, () => {
-//   console.log(`app is running on port ${PORT}`);
-// });
+//   console.log( `server listening on port: ${PORT}`);
+// })
+
+
+// // app.get('*', function(req, res) {
+// //   res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// // });
+
+// // app.listen(PORT, () => {
+// //   console.log(`app is running on port ${PORT}`);
+// // });
